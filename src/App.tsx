cb31db342d2af8a567689e491656ca9f30881206
Zsolt Dobak <zsolt.dobak@oracle.com>
2021-06-14 12:03:42 +0200

    Handle app settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {useSessionStorage} from 'react-use';
 import './styles/App.scss';
 import classNames from "classnames";
+import Switch from '@material-ui/core/Switch';
 
 import {AppSettingsContext, Theme, UnitType} from "./utility/appSettingsContext";
 
@@ -12,6 +13,16 @@ function App() {
   const [activeUnitType, setActiveUnitType] = useSessionStorage<UnitType>('active-unit-type', UnitType.METRIC);
   const [activeTheme, setActiveTheme] = useSessionStorage<Theme>('active-theme', Theme.LIGHT);
 
+  const handleUnitTypeChange = useCallback(() => {
+    if(activeUnitType === UnitType.METRIC) setActiveUnitType(UnitType.IMPERIAL);
+    else setActiveUnitType(UnitType.METRIC);
+  }, [activeUnitType]);
+
+  const handleThemeChange = useCallback(() => {
+    if(activeTheme === Theme.LIGHT) setActiveTheme(Theme.DARK);
+    else setActiveTheme(Theme.LIGHT);
+  }, [activeTheme]);
+
   return (
     <AppSettingsContext.Provider value={{
       unitType: activeUnitType ?? UnitType.METRIC,
@@ -21,7 +32,19 @@ function App() {
     }}>
       <div className={classNames('app', activeTheme)}>
         <header className="app-header">
-          <label>NN Weather App</label>
+          <h1>NN Weather App</h1>
+          <div className={'controls'}>
+            <div className={'control'} onClick={handleThemeChange}>
+              <Switch checked={activeTheme === Theme.DARK}
+                      size="small"/>
+              <label>Switch to {activeTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT} theme</label>
+            </div>
+            <div className={'control'} onClick={handleUnitTypeChange}>
+              <Switch checked={activeUnitType === UnitType.IMPERIAL}
+                      size="small"/>
+              <label>Switch to {activeUnitType === UnitType.METRIC ? UnitType.IMPERIAL : UnitType.METRIC} unit types</label>
+            </div>
+          </div>
         </header>
 
         <WidgetContainer />
